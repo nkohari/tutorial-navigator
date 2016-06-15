9abe25e1cfebbe109c127a5bc2068d89045f89c9
Nate Kohari <nkohari@gmail.com>
2016-06-15 09:31:44 -0400

    Added header with title and description to tutorial TOC sidebar

--- src/components/tutorial-table-of-contents.jsx
+++ src/components/tutorial-table-of-contents.jsx
@@ -32,9 +32,13 @@ class TutorialTableOfContents extends React.Component {
         {article.title}
       </li>
     });
-    
+
     return (
       <div className="tutorial-toc">
+        <div className="tutorial-toc-header">
+          <div className="tutorial-toc-title">{platform.title} Tutorials</div>
+          <div className="tutorial-toc-description">{platform.description}</div>
+        </div>
         <ul className="tutorial-toc-articles">
           {items}
         </ul>
