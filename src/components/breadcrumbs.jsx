f7eb678a08b6703150b8d8193f50e795e6adfff7
Guillermo Gonzalez Aleman <ggonzaleza@litebox.com.ar>
2016-07-22 20:05:07 -0300

    Added microdata to Breadcrumbs

--- src/components/breadcrumbs.jsx
+++ src/components/breadcrumbs.jsx
@@ -18,6 +18,7 @@ class Breadcrumbs extends React.Component {
   render() {
     let crumbs = [];
     let {quickstart, platform, article, isRestricted} = this.props;
+    let index = 1;
     
     if (!quickstart) {
       return <div />;
@@ -28,40 +29,63 @@ class Breadcrumbs extends React.Component {
     // top-level Documentation link.
     if (isRestricted) {
       crumbs.push(
-        <a key="quickstart" onClick={this.handleClick.bind(this, {quickstart})}>
-          <span className="text">{quickstart.title}</span>
-        </a>
+        <div itemProp="itemListElement" itemScope itemType="http://schema.org/ListItem">
+          <a itemProp="item" key="quickstart" onClick={this.handleClick.bind(this, {quickstart})}>
+            <span className="text" itemProp="name">{quickstart.title}</span>
+            <meta itemProp="position" content={index} />
+          </a>
+        </div>
       );
+      index++;
     }
     else {
       crumbs.push(
-        <a key="base" onClick={this.handleClick.bind(this, {})}>
-          <span className="text">Documentation</span>
-        </a>
+        <div itemProp="itemListElement" itemScope itemType="http://schema.org/ListItem">
+          <a itemProp="item" key="base" onClick={this.handleClick.bind(this, {})}>
+            <span className="text" itemProp="name">Documentation</span>
+            <meta itemProp="position" content={index} />
+          </a>
+        </div>
       );
+      index++;
+      crumbs.push(<i className="icon-budicon-461"></i>);
       crumbs.push(
-        <a key="quickstart" onClick={this.handleClick.bind(this, {quickstart})}>
-          <i className="icon-budicon-461"></i><span className="text">{quickstart.title}</span>
-        </a>
+        <div itemProp="itemListElement" itemScope itemType="http://schema.org/ListItem">
+          <a itemProp="item" key="quickstart" onClick={this.handleClick.bind(this, {quickstart})}>
+            <span className="text" itemProp="name">{quickstart.title}</span>
+            <meta itemProp="position" content={index} />
+          </a>
+        </div>
       );
+      index++;
     }
     
     if (platform) {
+      crumbs.push(<i className="icon-budicon-461"></i>);
       crumbs.push(
-        <a key="platform" onClick={this.handleClick.bind(this, {quickstart, platform})}>
-          <i className="icon-budicon-461"></i><span className="text">{platform.title}</span>
-        </a>
-      );
-      if (article && platform.articles.length > 1) {
-        crumbs.push(
-          <a key="article" onClick={this.handleClick.bind(this, {quickstart, platform, article})}>
-            <i className="icon-budicon-461"></i><span className="text">{article.title}</span>
+        <div itemProp="itemListElement" itemScope itemType="http://schema.org/ListItem">
+          <a itemProp="item" key="platform" onClick={this.handleClick.bind(this, {quickstart, platform})}>
+            <span className="text" itemProp="name">{platform.title}</span>
+            <meta itemProp="position" content={index} />
           </a>
+        </div>
+      );
+      index++;
+      if (article && platform.articles.length > 1) {
+        crumbs.push(<i className="icon-budicon-461"></i>);
+        crumbs.push(
+          <div itemProp="itemListElement" itemScope itemType="http://schema.org/ListItem">
+            <a itemProp="item" key="article" onClick={this.handleClick.bind(this, {quickstart, platform, article})}>
+              <span className="text" itemProp="name">{article.title}</span>
+              <meta itemProp="position" content={index} />
+            </a>
+          </div>
         );
+        index++;
       }
     }
     
-    return <div className="breadcrumbs">{crumbs}</div>;
+    return <div className="breadcrumbs" itemScope itemType="http://schema.org/BreadcrumbList">{crumbs}</div>;
   }
   
 }
